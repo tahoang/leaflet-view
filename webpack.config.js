5b016886089f314929ac943f3d43eebdd584047a
tahoang <dhoang519@gmail.com>
2018-10-04 16:06:14 -0400

    upgraded webpack and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,129 @@
-/*
-Tu Hoang
+//webpack 4 config
 
-Webpack config
+const path = require('path');
+const webpack = require('webpack');
+// const nodeExternals = require('webpack-node-externals');
+// const merge = require('webpack-merge');
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-*/
 
-var webpack = require('webpack');
-var path = require('path');
+//load config for each mode (dev/prod)
+// const dev = require('./config/webpack.dev.js');
+// const prod = require('./config/webpack.prod.js');
 
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = (env, argv) => {
+  let mode = env.production ? 'production' : 'development';
+  console.log(mode);
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve('./dist'),
-    filename: 'leaflet-view.js'
-  },
-  module: {
-    rules: [
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' }
-      // , {
-      //   test: /\.less$/,
-      //   use: ExtractTextPlugin.extract({
-      //     fallback: 'style-loader',
-      //     use: ['css-loader', 'less-loader']
-      //   })
-      // }
-      , {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader']
-        })
+  const common = {
+    mode,
+    // context: path.resolve(__dirname, 'component'),
+    entry: {
+      index: path.resolve(__dirname, 'src/index.js')
+    },
+    output: {
+      libraryTarget: 'umd',
+      library: {
+        commonjs: 'dashboard-react-components',
+        root: 'dashboard-react-components'
       },
-      {
-        test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
-        use: [{
-          loader: "file-loader"
-        }]
+      filename: '[name].js',
+      // chunkFilename: '[name].chunk.js',
+      path: path.resolve(__dirname, './dist')
+      // publicPath: 'dist/'
+    },
+    // target: 'web',
+    // externals: [nodeExternals()],
+    externals: {
+      leaflet: 'leaflet',
+      lodash: 'lodash'
+    },
+    // optimization: {
+    //    splitChunks: {
+    //      cacheGroups: {
+    //        commons: {
+    //          test: /[\\/]node_modules[\\/]/,
+    //          name: 'vendors',
+    //          chunks: 'all'
+    //          // priority: -10
+    //        },
+    //        default: {
+    //          minChunks: 2,
+    //          priority: -20,
+    //          reuseExistingChunk: true
+    //        }
+    //      }
+    //    }
+    //  },
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+
       }, {
-        test: /\.(jpe?g|png|gif|svg)$/i,
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      }, {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'url-loader?limit=10000',
-          'img-loader'
+          'file-loader'
         ]
-      }
+      }, {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader',
+        options: {
+          // helperDirs: path.join(__dirname, 'src/client/helpers'),
+          precompileOptions: {
+            knownHelpersOnly: false,
+          }
+        }
+      },/*{
+        test: /\.(le|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './'
+            }
+          },
+          'css-loader',
+          // 'postcss-loader',
+          'less-loader',
+        ],
+      },*/
+      {
+        test: /\.(le|sc|c)ss$/,
+        use: [
+          // 'style-loader',
+          'css-loader',
+          // 'postcss-loader',
+          // 'less-loader',
+        ]
+      }]
+    },
+    plugins: [
+      // new CleanWebpackPlugin(['public/dist']),
+      //global constants for developement/production mode
+      // new webpack.DefinePlugin({
+      //  'process.env': {
+      //      NODE_ENV: JSON.stringify(env.production ? 'production' : 'development')
+      //    }
+      // })
+
     ]
-  },
-  plugins: [
-    new ExtractTextPlugin({ //for css extraction
-      filename: "./leaflet-view-style.css",
-      // disable: process.env.NODE_ENV === "development",
-      allChunks: true
-    })
-    // new webpack.optimize.UglifyJsPlugin({
-    //   beautify: false,
-    //   mangle: {
-    //     screw_ie8: true,
-    //     keep_fnames: true
-    //   },
-    //   compress: {
-    //     screw_ie8: true
-    //   },
-    //   comments: false
-    // })
-  ]
+  };
+
+  return common;
+  // if(env.production) 
+  //  return merge(common, prod);
+  // else
+  //  return merge(common, dev);
+
 };
